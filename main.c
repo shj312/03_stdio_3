b3e3cf17c40c0d0582e8996f40f62c602899fc57
shj312 <sshj312@naver.com>
2020-09-17 16:27:12 +0900

    03_stdio_3

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	char c;
+	
+	printf("input an alphabet :");
+	scanf("%c", &c);
+	
+	printf("input alphabet is %c (%d), and the next one is %c (%d) \n", c, c, c+1, c+1);
+	
+	return 0;
+}
